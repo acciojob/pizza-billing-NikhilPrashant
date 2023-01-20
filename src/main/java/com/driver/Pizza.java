b0028aa9cbbdd22900485eb6ea897d5d43aab19f
nikhilprashant <nikhilprashant1998@gmail.com>
2023-01-20 19:54:32 +0530

    Solved 1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,26 +9,48 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if (isVeg) price = 300;
+        else price = 400;
     }
 
     public int getPrice(){
         return this.price;
     }
 
+    boolean isCheeseAdded = false;
     public void addExtraCheese(){
         // your code goes here
+        if (!isCheeseAdded) {
+            price += 80;
+            isCheeseAdded = true;
+        }
     }
 
+    boolean areToppingsAdded = false;
     public void addExtraToppings(){
         // your code goes here
+        if (!areToppingsAdded) {
+            price += isVeg? 70 : 120;
+            areToppingsAdded = true;
+        }
     }
 
+    boolean isTakeAwayAdded = false;
     public void addTakeaway(){
         // your code goes here
+        if (!isTakeAwayAdded) {
+            price += 20;
+            isTakeAwayAdded = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        bill = "Base Price Of The Pizza: " + (isVeg? 300 : 400)
+                + (isCheeseAdded? "\nExtra Cheese Added: " + 80 : "")
+                + (areToppingsAdded ? "\nExtra Toppings Added: " + (isVeg ? 70: 120) : "")
+                + (isTakeAwayAdded ? "\nPaperbag Added: " + 20 : "")
+                + "\nTotal Price: " + price;
         return this.bill;
     }
 }
